90151cd1942e27c59fc05d1b59b2d4b6b40e5b67
Emanuele Corona <emanuele.corona@primaton.it>
2024-07-09 16:15:45 +0200

    Added @Getter and @Setter annotation to PokemonEntity

--- src/main/java/it/corona/unitest/model/entity/PokemonEntity.java
+++ src/main/java/it/corona/unitest/model/entity/PokemonEntity.java
@@ -1,11 +1,11 @@
 package it.corona.unitest.model.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
